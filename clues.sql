8ecc35f4377cc5d6903e9e5fcc98462f35843791
rhiannon-obrien-coder <rhiannonobrien@gmail.com>
2024-12-02 15:20:30 -0600

    clues 5 and 6 completed

--- clues.sql
+++ clues.sql
@@ -42,21 +42,71 @@ WHERE code = (
 -- flying to.
 
 -- Write SQL query here
-
+SELECT *
+FROM cities
+WHERE countrycode = (
+    SELECT countrycode
+    FROM countrylanguages 
+    WHERE language = (
+        SELECT language
+        FROM countrylanguages
+        WHERE countrycode = (
+            SELECT code 
+            FROM countries 
+            WHERE region = 'Southern Europe'
+            AND population = (
+                SELECT MIN(population) 
+                FROM countries 
+                WHERE region = 'Southern Europe'
+        )))
+        AND percentage = 100)
+AND name !=
+(SELECT name
+FROM countries 
+WHERE code = (
+    SELECT countrycode
+    FROM countrylanguages 
+    WHERE language = (
+        SELECT language
+        FROM countrylanguages
+        WHERE countrycode = (
+            SELECT code 
+            FROM countries 
+            WHERE region = 'Southern Europe'
+            AND population = (
+                SELECT MIN(population) 
+                FROM countries 
+                WHERE region = 'Southern Europe'
+        )))
+        AND percentage = 100));
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different
 -- parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we
 -- were headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
 
 -- Write SQL query here
-
+SELECT *
+FROM cities
+WHERE name 
+LIKE 'Serra%'
+AND name!= 'Serravalle';
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
 -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
 -- follow right behind you!
 
 -- Write SQL query here
-
+SELECT *
+FROM cities
+WHERE id =(
+    SELECT capital
+    FROM countries
+    WHERE code = (
+        SELECT countrycode
+        FROM cities
+        WHERE name 
+        LIKE 'Serra%'
+        AND name!= 'Serravalle'));
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to
 -- the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the
